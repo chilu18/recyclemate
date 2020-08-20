41d541afc9ffc9eceef2fdbd8457ba7031f65b25
Chilumba Machona <chilumba@Chilumbas-MacBook-Pro.local>
2020-08-20 23:33:14 +0800

    script3

--- recyclemate.py
+++ recyclemate.py
@@ -1,6 +1,9 @@
 from gpiozero import LED
 from gpiozero import MotionSensor
+from picamera import PiCamera
+from time import sleep
 
+camera = PiCamera()
 green_led = LED(17)
 pir = MotionSensor(4)
 green_led.off()
@@ -9,6 +12,10 @@ while True:
     pir.wait_for_motion()
     green_led.on()
     print("Motion Detected")
+    camera.start_preview()
+    sleep(5)
+    camera.capture('/home/pi/Desktop/image.jpg')
+    camera.stop_preview()
     pir.wait_for_no_motion()
     green_led.off()
     print("Motion Stopped")
